aedbd18eba001a764b418f5b182150329e952cc5
Khalid <114188229+sunsetofmoon@users.noreply.github.com>
2023-01-18 17:13:51 +0300

    first commit

---
+++ src/Base.jsx
@@ -0,0 +1,12 @@
+import Expo from './public/Expo.png'
+
+function Base() {
+    return (
+        <div>
+            <h1>Tyler Durden</h1>
+            <img className='img' src={Expo} alt="" />
+        </div>
+    )
+}
+
+export default Base;
